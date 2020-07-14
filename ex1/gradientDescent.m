9f769491613271bc9ded932c92bdcf31a66e0ac7
Pushpak Patil <49149855+pushpak4166@users.noreply.github.com>
2020-07-14 16:17:21 +0530

    Add files via upload

---
+++ ex1/gradientDescent.m
@@ -0,0 +1,26 @@
+
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    sum1 = sum(theta'.*X,2);
+    sum2 = (sum1 - y);
+    theta = theta - (alpha/m)*[ sum(sum2.*X(:,1)) ; sum(sum2.*X(:,2)) ] ; 
+
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
+
+end
+
+end
